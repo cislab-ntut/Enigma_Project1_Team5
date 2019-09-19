5783507e14cd6d97ec634680211530c82ec7c2fa
changjackj <51290226+changjackj@users.noreply.github.com>
2019-09-19 21:40:01 +0800

    Add files via upload

---
+++ readfile.py
@@ -0,0 +1,13 @@
+alphet_to_num = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,
+				"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25}
+def read_file(filename):
+	global alphet_to_num
+	f = open(filename, "r")
+	lines = f.readlines()
+	string = ""
+	for line in lines:
+		line.strip('\n')
+		string = string+line
+	lst = list(string)
+	file = [alphet_to_num[char] for char in lst]
+	return file
