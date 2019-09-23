407cceb017b7073ad85db58bb87b2b0c73db61a2
Sebastian <godofyax@gmail.com>
2019-09-23 20:34:25 +0800

    Update enigma.py

--- enigma.py
+++ enigma.py
@@ -5,9 +5,6 @@ alphet_to_num = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K"
 num_to_alphet = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J",10:"K",11:"L",12:"M",13:"N",14:"O",15:"P",16:"Q",17:"R",18:"S",
 				19:"T",20:"U",21:"V",22:"W",23:"X",24:"Y",25:"Z"}
 
-<<<<<<< HEAD
-	
-=======
 def rotate(rotor):
 	tmp_r = rotor.pop(0)
 	rotor.append(tmp_r)
@@ -93,7 +90,7 @@ def main():
 	code = encode(input)
 	output = [num_to_alphet[num] for num in code]
 	print(output)
->>>>>>> f65d6a38caac08b570013333427fdbd58e43e9d2
+
 
 if __name__ == "__main__":
 	main()
